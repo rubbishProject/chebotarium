a9d6d281d399e019be4fec8f4de077194d42703b
andreybrsv <andreybrsv@ya.ru>
2020-01-04 14:19:38 +0300

    her

---
+++ src/main/java/core/Controller.java
@@ -0,0 +1,14 @@
+package core;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String getString() {
+
+        return "huy";
+    }
+}
