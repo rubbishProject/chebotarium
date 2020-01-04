7e7a92e74fbe29a54edd8a94b90648be350561c7
andreybrsv <andreybrsv@ya.ru>
2020-01-04 13:48:31 +0300

    make it better with heroku

---
+++ src/main/java/core/ChebotariumBot.java
@@ -0,0 +1,31 @@
+package core;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class ChebotariumBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+
+            try {
+                execute(message);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "ChebotariumBot";
+    }
+
+    public String getBotToken() {
+        return "1021570162:AAH1Bb3SxCWpHaVPYnGarrKFAjogpjEmpFk";
+    }
+}
